32e7246f57647fbbb8748427d35891e098f2214e
Andrew Nguyen <anguyen@doubledutch.me>
2014-07-09 09:40:16 -0700

    Modified timestamp

--- autovpncscript.sh
+++ autovpncscript.sh
@@ -4,10 +4,10 @@ pid=`ps aux | grep [v]pnc-connect | awk '{print $2}'`
 
 if [ $pid ]
 then
-  echo $(date +%Y-%m-%d:%T) Connection already established with PID $pid
+  echo $(date +%Y-%m-%d\ %T) Connection already established with PID $pid
 else
   sudo vpnc-connect rackspace --local-port 0
   pid=`ps aux | grep [v]pnc-connect | awk '{print $2}'`
-  echo $(date +%Y-%m-%d:%T) Connection established with PID $pid 
+  echo $(date +%Y-%m-%d\ %T) Connection established with PID $pid 
 fi
 
