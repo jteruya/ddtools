7e9a37a0a42b5e8552fc5e95fefdb7302f710051
Andrew Nguyen <anguyen@doubledutch.me>
2014-07-08 23:56:44 -0700

    Assigned pid variable correctly

--- autovpncscript.sh
+++ autovpncscript.sh
@@ -7,6 +7,7 @@ then
   echo $(date +%Y-%m-%d:%T) Connection already established with PID $pid
 else
   sudo vpnc-connect rackspace --local-port 0
+  pid=`ps aux | grep [v]pnc-connect | awk '{print $2}'`
   echo $(date +%Y-%m-%d:%T) Connection established with PID $pid 
 fi
 
