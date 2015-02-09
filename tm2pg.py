395c2d3359481ff4fd3c70628c521ad6030256f9
Kevin Chiou <kevinchiou@DDs-MBP-4.doubledutch.me>
2015-02-09 15:35:19 -0800

    Changed NULL paramter for Copy_From

--- tm2pg.py
+++ tm2pg.py
@@ -102,7 +102,7 @@ def main():
   conn = psycopg2.connect("dbname='dev' user='" + pg_user + "' host='10.223.176.60' port='5432'")
 
   # Copy from file to Postgres Table
-  conn.cursor().copy_from(f,pg_schema_table,sep='~')
+  conn.cursor().copy_from(f,pg_schema_table,sep='~',null='None')
   conn.commit()
 
   # Close the landed file
